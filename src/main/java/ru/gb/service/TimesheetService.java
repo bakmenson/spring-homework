161a33cdad332194fa887b6a02eceaf5d1fc9883
bakmenson <bakmenson@gmail.com>
2024-07-05 17:53:46 +0300

    add necessary packages

---
+++ src/main/java/ru/gb/service/TimesheetService.java
@@ -0,0 +1,35 @@
+package ru.gb.service;
+
+import org.springframework.stereotype.Service;
+import ru.gb.model.Timesheet;
+import ru.gb.repository.TimesheetRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service // то же самое, что и Component
+public class TimesheetService {
+
+  private final TimesheetRepository repository;
+
+  public TimesheetService(TimesheetRepository repository) {
+    this.repository = repository;
+  }
+
+  public Optional<Timesheet> getById(Long id) {
+    return repository.getById(id);
+  }
+
+  public List<Timesheet> getAll() {
+    return repository.getAll();
+  }
+
+  public Timesheet create(Timesheet timesheet) {
+    return repository.create(timesheet);
+  }
+
+  public void delete(Long id) {
+    repository.delete(id);
+  }
+
+}
