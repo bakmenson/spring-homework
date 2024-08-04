6533d1e12468abbfba37b68fa2981b951b67a200
bakmenson <bakmenson@gmail.com>
2024-08-04 17:37:47 +0300

    add logging all methods with args types and values

---
+++ src/main/java/ru/gb/aspect/LoggingAspect.java
@@ -0,0 +1,40 @@
+package ru.gb.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@Aspect
+public class LoggingAspect {
+
+    @Before("within(ru.gb..*)")
+    public void methodsLogging(JoinPoint jp) {
+        StringBuilder args = new StringBuilder();
+
+        MethodSignature methodSignature = (MethodSignature) jp.getSignature();
+
+        var parameterTypes = methodSignature.getParameterTypes();
+
+        for (int i = 0; i < parameterTypes.length; i++) {
+            args.append(parameterTypes[i].getSimpleName())
+                    .append(" = ")
+                    .append(jp.getArgs()[i])
+                    .append(", ");
+        }
+
+        if (!args.isEmpty()) {
+            args.delete(args.length() - 2, args.length());
+        }
+
+        String targetClassName = jp.getTarget().getClass().getSimpleName();
+        String targetMethodName = jp.getSignature().getName();
+
+        log.info(String.format("Log: %s.%s(%s)", targetClassName, targetMethodName, args));
+    }
+
+}
