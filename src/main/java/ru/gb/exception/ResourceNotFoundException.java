d1fa746bc8160a0c4523c32956578201b952995d
bakmenson <bakmenson@gmail.com>
2024-08-08 22:09:39 +0300

    remove unnecessary code

--- src/main/java/ru/gb/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.gb.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
